9d726a04b9af7206eef39da1f0a6884ab72efd24
cherrypicker1122 <nrkim1122@gmail.com>
2023-12-28 23:31:49 +0900

    center align

--- src/app/pages/daily/daily-list/daily-list/daily-list.component.ts
+++ src/app/pages/daily/daily-list/daily-list/daily-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DailyModel } from './daily-list.model';
 
 @Component({
@@ -6,33 +6,57 @@ import { DailyModel } from './daily-list.model';
   templateUrl: './daily-list.component.html',
   styleUrls: ['./daily-list.component.scss']
 })
-export class DailyListComponent {
+export class DailyListComponent implements OnInit {
 
   dailyList : Array<DailyModel> = [
     {
-      color : "projcard projcard-blue",
-      img: "https://th.bing.com/th/id/OIP.Q9KUnLeSoZxUv2U7jvL2vQHaHa?rs=1&pid=ImgDetMain", // https://picsum.photos/800/600?image=1041
+      img: "https://th.bing.com/th/id/OIP.4TmWsjC7iC0WK27mxTGbOgHaEK?rs=1&pid=ImgDetMain", // https://picsum.photos/800/600?image=1041
+      title: "20231228",
+      subtitle: "",
+      description: "되돌아보니 부끄럽고 챙피하다 \n ",
+      tagList : [],
+      date : new Date(),
+      dayago: ""
+    },
+    {
+      img: "https://th.bing.com/th/id/OIP.4TmWsjC7iC0WK27mxTGbOgHaEK?rs=1&pid=ImgDetMain", // https://picsum.photos/800/600?image=1041
       title: "20231224",
       subtitle: "",
       description: "이번 한 해 정말 많은 일이 있었지 \n \
       그래도 몸이 많이 나아져서 2024년 부터는 \n \
       더 이상 지긋지긋하게 맞던 주사도 안 맞아도 되고 병원도 안 다녀도 된다 \n \
-      나한테 주어진 시간이 얼마 안남았으니까 \n \
+      나한테 주어진 시간이 얼마 안 남았으니까 \n \
       열심히 살자  \n \
       근데 프론트 앤드는 하기도 싫고 배우기도 싫다",
       tagList : [],
-      dayago: "1 days ago"
+      date : new Date('2023-12-24'),
+      dayago: ""
     },
     {
-      color : "projcard projcard-red",
-      img: "https://th.bing.com/th/id/OIP.Q9KUnLeSoZxUv2U7jvL2vQHaHa?rs=1&pid=ImgDetMain", //https://picsum.photos/800/600?image=1080
+      img: "https://th.bing.com/th/id/OIP.4TmWsjC7iC0WK27mxTGbOgHaEK?rs=1&pid=ImgDetMain", //https://picsum.photos/800/600?image=1080
       title: "20231218",
       subtitle: "",
       description: "조금 더 나아질 수 있을까 \n \
       탈출 할 수 있을까",
       tagList : [],
-      dayago: "12 days ago"
+      date : new Date('2023-12-18'),
+      dayago: ""
     }
   ]
+
+  ngOnInit() {
+
+    for(var daily of this.dailyList){
+      daily.dayago = this.getDaysAgo(daily.date) + " days ago";
+    }
+  }
+
+  getDaysAgo(date: Date){
+    return String(Math.round((+new Date() - +date) / (1000 * 3600 * 24))); 
+  }
   
 }
+function moment(arg0: string): any {
+  throw new Error('Function not implemented.');
+}
+
--- src/app/pages/daily/daily-list/daily-list/daily-list.model.ts
+++ src/app/pages/daily/daily-list/daily-list/daily-list.model.ts
@@ -1,10 +1,10 @@
 
 export interface DailyModel {
-    color : string
     img : string
     title : string
     subtitle : string
     description : string
     tagList : Array<string>
     dayago: string
+    date: Date
 }
